43e63061a126fe15bb8f111a72ead77c5d0f47b3
vrncff <126357429+vrncff@users.noreply.github.com>
2024-05-21 11:41:57 -0300

    Add libGDX empty project

---
+++ libgdx-project/desktop/src/org/group5/snake/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package org.group5.snake;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.group5.snake.SnakeGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Snake Game");
+		new Lwjgl3Application(new SnakeGame(), config);
+	}
+}
