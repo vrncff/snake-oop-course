8a976918632c94d4da80ec883b84c1e7448577a8
Oliver Kobayashi <105022371+oliverkobayashi@users.noreply.github.com>
2024-05-29 14:55:59 -0300

    v1.1
    tasks e possiveis features para o jogo

--- SnakeGame/core/src/com/mygdx/game/Game.java
+++ SnakeGame/core/src/com/mygdx/game/Game.java
@@ -7,6 +7,31 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Entities.Snake;
 import com.mygdx.game.Entities.Apple;
 
+/**
+ * TASKS:
+ * 	1. Tela de início
+ * 		1.1 Design
+ * 		1.2	Single PLayer
+ * 		2.3 Multiplayer
+ * 	2. Iniciar o jogo com a cobra parada
+ * 	3. Fazer ela crescer conforme coleta as maçãs
+ * 	4. Velocidade da cobra aumenta gradativamente
+ * 	5. Implementar o game over
+ * 		5.1 Colisão com a parede
+ * 		5.2 Colisão com ela mesma
+ * 		5.3 Tela de game over
+ * 	6. Implementar um scoreboard
+ * 	7. Sons e música
+ * 	8. Implementar o Multiplayer
+ * 	9. Comentários em JAVADOC (em inglês)
+ * FEATURES:
+ *	1.	Animação para a maçã
+ *	2.	Animação ao comer a maçã
+ *	3.	Animação para a boca a cobra ao se aproximar da maçã
+ */
+
+
+
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture imgBackGroung, imgSnake, imgApple;
